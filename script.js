241eed29072bbc6797a2bb195d5f5d67a7443ea1
williamwestergard <williamwestergardd@gmail.com>
2024-12-16 12:22:03 +0100

    mer meny innehåll och en kombination av menykorten i css och javascript

--- script.js
+++ script.js
@@ -5,48 +5,48 @@ items.forEach((item, index) => {
   const article = document.createElement("article");
   article.classList.add("menu-card");
 
-  // Create the image
+  if (index === 0) {
+    article.classList.add("menu-card-first");
+  }
+
+  // Create Image
   const img = document.createElement("img");
   img.classList.add("menu-image");
   img.src = item.img;
   img.alt = item.name;
 
-  // Create the info container
+  // Create info container
   const infoContainer = document.createElement("section");
   infoContainer.classList.add("menu-info-container");
 
-  // Create the title
+  // Create title
   const title = document.createElement("section");
   title.classList.add("menu-title");
   title.innerHTML = item.name;
 
-  // Create the description
+  // Create description
   const desc = document.createElement("section");
   desc.classList.add("menu-desc");
   desc.innerHTML = item.dsc;
 
-  // Create the price and button container
+  // Create price and button container
   const priceContainer = document.createElement("section");
   priceContainer.classList.add("menu-price");
-  priceContainer.innerHTML = `${item.price} kr`;
+  const price = item.price;
+  const roundedUpPrice = Math.ceil(price);
+  priceContainer.innerHTML = `${roundedUpPrice} kr`;
 
   // Add Item button
   const button = document.createElement("button");
   button.classList.add("menu-add-item");
   button.textContent = "Lägg till";
 
-  // Append button to price container
+  // Append menu card content
   priceContainer.appendChild(button);
-
-  // Append all elements to info container
   infoContainer.appendChild(title);
   infoContainer.appendChild(desc);
   infoContainer.appendChild(priceContainer);
-
-  // Append image and info container to article
   article.appendChild(img);
   article.appendChild(infoContainer);
-
-  // Append the article to the main container
   menuContainer.appendChild(article);
 });
