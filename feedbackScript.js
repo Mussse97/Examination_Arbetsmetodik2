9a26d24b2a5b8bb4524fe208a037951a4708edfa
Sara <sara_lunden1@hotmail.com>
2024-12-17 11:28:00 +0100

    new files
    skapade nya filer för feedback sidan

---
+++ feedbackScript.js
@@ -0,0 +1,8 @@
+const body = document.body
+const divFood = document.getElementById("foodRate")
+const divRest = document.getElementById("restRate")
+const submitBtn = document. getElementById("submitbutton")
+
+const imgContainer1 = document.createElement("img")
+const imgContainer2 = document.createElement("img")
+
